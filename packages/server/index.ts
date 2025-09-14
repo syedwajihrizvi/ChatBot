9790f1b89c52ef6277aa7002aad7a62ffd0fad75
AB Rizvi <abrizvi@ABs-MacBook-Pro.local>
2025-09-14 17:49:08 -0400

    Add .env

---
+++ packages/server/index.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import type { Request, Response } from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello, World!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
